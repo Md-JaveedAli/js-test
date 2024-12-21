f0a2fb141f288bcbbeda3ae5dfaecca4ca98cb21
Mohd Javeed Ali <91204724+Md-JaveedAli@users.noreply.github.com>
2024-12-21 15:12:35 +0000

    okey

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountEmail = "javeed@gmail.com"
+var password = "hello"
+accountCity = "Hyderabad"
+
+accountEmail = "muqeed@gmail.com"
+password = "hai"
+accountCity = "MBNR"
+
+console.table([accountId,accountEmail,password,accountCity])
