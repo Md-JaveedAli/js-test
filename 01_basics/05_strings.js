fbc240dd4451e0aebc79d04512a373398f631bad
Mohd Javeed Ali <91204724+Md-JaveedAli@users.noreply.github.com>
2025-01-01 12:11:44 +0000

    Added 05 and 06

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,17 @@
 const name = "Javeed"
 const repoCount = 8
 
-console.log(`My name is "${name.toUppercase}" and my repo count is ${repoCount}`)
+//console.log(`My name is "${name}" and my repo count is ${repoCount}`)
+
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+//console.log(name.charAt(4))
+// console.log(name.indexOf('z'))
+//console.log(name.substring(1, 3))  // av
+
+const newstring = "          Javeed";
+//console.log(newstring)
+//console.log(newstring.trim()) -> removes extra space
+const url = "https://javeed.com/javeed%20ali"
+console.log(url.replace('%20','-'))
